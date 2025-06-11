1582a487b6c3ef3f7a0a75b21c181d75719dcfcc
mike <mike@haha.me>
2025-06-11 13:29:01 +0000

    feat: add comments

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -5,11 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /** Main endpoint to track visits. This one needs to be improved to be more performant. */
   @Get()
   trackVisits() {
     return this.appService.trackVisits();
   }
 
+  /** Endpoint to get the value from the database. Just for testing purposes. */
   @Get('db')
   getValueFromDB() {
     return this.appService.getValueFromDB();
